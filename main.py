8a82c9d64f52bfc1d4eefd761b64606a9a427adf
Nate <natejms@outlook.com>
2017-06-13 11:08:13 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from ampersand import build # Import all the needed modules
+
+def main(content): # Declare the starting point for your plugin
+  
+  # Preform your operations
+  print("Hello, World!")
+  
+  return content # Pass the content on to the next plugin
